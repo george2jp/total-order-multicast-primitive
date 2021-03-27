5cbf8f4e985623c1e0c41a964bd1b18f05c0214a
george2jp <41213983+george2jp@users.noreply.github.com>
2021-03-27 23:52:53 +1300

    Add files via upload

---
+++ Middleware1/Middleware1/Middleware1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Middleware1
+{
+    partial class Middleware1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.sentBox = new System.Windows.Forms.ListBox();
+            this.receivedBox = new System.Windows.Forms.ListBox();
+            this.readyBox = new System.Windows.Forms.ListBox();
+            this.sentLabel = new System.Windows.Forms.Label();
+            this.receivedLabel = new System.Windows.Forms.Label();
+            this.readyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sendButton.Location = new System.Drawing.Point(36, 30);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(163, 52);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // sentBox
+            // 
+            this.sentBox.FormattingEnabled = true;
+            this.sentBox.ItemHeight = 16;
+            this.sentBox.Location = new System.Drawing.Point(31, 147);
+            this.sentBox.Name = "sentBox";
+            this.sentBox.Size = new System.Drawing.Size(207, 260);
+            this.sentBox.TabIndex = 1;
+            // 
+            // receivedBox
+            // 
+            this.receivedBox.FormattingEnabled = true;
+            this.receivedBox.ItemHeight = 16;
+            this.receivedBox.Location = new System.Drawing.Point(265, 147);
+            this.receivedBox.Name = "receivedBox";
+            this.receivedBox.Size = new System.Drawing.Size(272, 260);
+            this.receivedBox.TabIndex = 2;
+            // 
+            // readyBox
+            // 
+            this.readyBox.FormattingEnabled = true;
+            this.readyBox.ItemHeight = 16;
+            this.readyBox.Location = new System.Drawing.Point(561, 147);
+            this.readyBox.Name = "readyBox";
+            this.readyBox.Size = new System.Drawing.Size(212, 260);
+            this.readyBox.TabIndex = 3;
+            // 
+            // sentLabel
+            // 
+            this.sentLabel.AutoSize = true;
+            this.sentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sentLabel.Location = new System.Drawing.Point(89, 111);
+            this.sentLabel.Name = "sentLabel";
+            this.sentLabel.Size = new System.Drawing.Size(61, 26);
+            this.sentLabel.TabIndex = 4;
+            this.sentLabel.Text = "Sent";
+            // 
+            // receivedLabel
+            // 
+            this.receivedLabel.AutoSize = true;
+            this.receivedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.receivedLabel.Location = new System.Drawing.Point(333, 111);
+            this.receivedLabel.Name = "receivedLabel";
+            this.receivedLabel.Size = new System.Drawing.Size(111, 26);
+            this.receivedLabel.TabIndex = 5;
+            this.receivedLabel.Text = "Received";
+            // 
+            // readyLabel
+            // 
+            this.readyLabel.AutoSize = true;
+            this.readyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.readyLabel.Location = new System.Drawing.Point(625, 111);
+            this.readyLabel.Name = "readyLabel";
+            this.readyLabel.Size = new System.Drawing.Size(80, 26);
+            this.readyLabel.TabIndex = 6;
+            this.readyLabel.Text = "Ready";
+            this.readyLabel.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // Middleware1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.readyLabel);
+            this.Controls.Add(this.receivedLabel);
+            this.Controls.Add(this.sentLabel);
+            this.Controls.Add(this.readyBox);
+            this.Controls.Add(this.receivedBox);
+            this.Controls.Add(this.sentBox);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Middleware1";
+            this.Text = "Middleware 1";
+            this.Load += new System.EventHandler(this.Middleware1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ListBox sentBox;
+        private System.Windows.Forms.ListBox receivedBox;
+        private System.Windows.Forms.ListBox readyBox;
+        private System.Windows.Forms.Label sentLabel;
+        private System.Windows.Forms.Label receivedLabel;
+        private System.Windows.Forms.Label readyLabel;
+    }
+}
+
---
+++ Middleware3/Middleware3/Middleware3.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Middleware3
+{
+    public partial class Middleware3 : Form
+    {
+        const int myPort = 8084;
+        int Id = 0;
+        private int counter = 0;
+        private string timeStamp = "";
+        private string data = null;
+        int[] vector = new int[5];
+        int[] flag = new int[5];
+        int number = 0;
+        ArrayList readyList = new ArrayList();
+        ArrayList ports = new ArrayList();
+        ArrayList priorityQ = new ArrayList();
+        ArrayList localQ = new ArrayList();
+
+        IComparer dataSort = new FilesNameComparerClass();
+        // spliter spliter = new spliter();
+        public Middleware3()
+        {
+            InitializeComponent();
+            ReceiveMulticast();
+        }
+
+        private void Middleware1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+        public void Start()
+        {
+            number += 1;
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = null;
+                foreach (IPAddress ip in ipHostInfo.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip;
+                        break;
+                    }
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8081);
+                Socket sendSocket;
+                try
+                {
+                    sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    sendSocket.Connect(remoteEP);
+                    Random random = new Random();
+                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    string cont = new string(Enumerable.Repeat(chars, 3).Select(s => s[random.Next(s.Length)]).ToArray());
+                    byte[] msg = Encoding.ASCII.GetBytes("Msg from," + myPort + ",#" + number +cont+ "<EOM>");
+                    int bytesSent = sendSocket.Send(msg);
+                    sendSocket.Shutdown(SocketShutdown.Both);
+                    sendSocket.Close();
+                    sentBox.Items.Add("Msg "+cont+" from," + myPort + ",<EOM>");
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException: {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception:{0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private async void ReceiveMulticast()
+        {
+            byte[] bytes = new Byte[1024];
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = null;
+            foreach (IPAddress ip in ipHostInfo.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                    break;
+                }
+            }
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, myPort);
+            TcpListener listener = new TcpListener(localEndPoint);
+            listener.Start(10);
+            try
+            {
+                while (true)
+                {
+                    TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+                    data = null;
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        NetworkStream readStream = tcpClient.GetStream();
+                        int bytesRec = await readStream.ReadAsync(bytes, 0, 1024);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOM>") > -1)
+                        {
+                            break;
+                        }
+                    }
+                    if (data.Contains("&"))
+                    {
+                        string[] tempMsg = data.Split('&');
+                        int port = Int32.Parse(tempMsg[1].Substring(tempMsg[1].IndexOf('*') + 1, 4));
+                        ports.Add(port);
+                        string empty = "";
+                        localQ.Add(tempMsg[1]);
+                        localQ.Sort(dataSort);
+                        string maxOne = "Max:" + localQ[0].ToString();
+                        if (localQ.Count == 5)
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                sendBack(maxOne, Int32.Parse(ports[i].ToString()), empty);
+                            }
+                            localQ.Clear();
+                        }
+                        
+                    }
+                    else if (data.Contains("Max:"))
+                    {
+                        string[] theOne = data.Split(':');
+                        string[] maxTimeStampArray = theOne[1].Split('*');
+                        if (priorityQ.Count != 0)
+                        {
+                            for (int i = 0; i < priorityQ.Count; i++)
+                            {
+                                string[] fromPQ = priorityQ[i].ToString().Split('*');
+                                if (fromPQ[2] == maxTimeStampArray[2])
+                                {
+                                    fromPQ[0] = maxTimeStampArray[0];
+                                    fromPQ[1] = maxTimeStampArray[1];
+                                    readyBox.Items.Add(priorityQ[i].ToString().Substring(priorityQ[i].ToString().LastIndexOf('*') + 1));
+                                    priorityQ.Remove(priorityQ[i]);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        receivedBox.Items.Add("msg received: " + data);
+                        counter += 1;
+                        string[] tempData = data.Split(',');
+                        int portToSend = Int32.Parse(tempData[1]);
+                        string finalData = "";
+                        for (int i = 0; i < tempData.Length; i++)
+                        {
+                            finalData += tempData[i] + ",";
+                        }
+                        timeStamp = counter + "*" + myPort;
+                        string priorityData = timeStamp + "*" + finalData;
+                        priorityQ.Add(priorityData);
+                        DoWork(priorityData, portToSend);
+                    }
+
+                }
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+        }
+
+        public void DoWork(string data, int port)
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = null;
+                foreach (IPAddress ip in ipHostInfo.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip;
+                        break;
+                    }
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+                Socket sendSocket;
+                try
+                {
+                    sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    sendSocket.Connect(remoteEP);
+
+                    byte[] msg = Encoding.ASCII.GetBytes("&" + data);
+
+                    int bytesSent = sendSocket.Send(msg);
+
+                    sendSocket.Shutdown(SocketShutdown.Both);
+                    sendSocket.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+
+        public void sendBack(string maxOne, int port, string empty)
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = null;
+                foreach (IPAddress ip in ipHostInfo.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipAddress = ip;
+                        break;
+                    }
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+                Socket sendSocket;
+                try
+                {
+                    sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    sendSocket.Connect(remoteEP);
+                    byte[] msg = Encoding.ASCII.GetBytes(maxOne);
+                    int byteSent = sendSocket.Send(msg);
+                    sendSocket.Shutdown(SocketShutdown.Both);
+                    sendSocket.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+
+    }
+
+    internal class FilesNameComparerClass : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            if (x==null || y == null)
+            {
+                throw new ArgumentException("Parameters can not be null");
+            }
+            string fileA = x as string;
+            string fileB = y as string;
+            char[] arr1 = fileA.ToCharArray();
+            char[] arr2 = fileB.ToCharArray();
+            int i = 0, j = 0;
+            while(i<arr1.Length && j < arr2.Length)
+            {
+                if (char.IsDigit(arr1[i]) && char.IsDigit(arr2[j]))
+                {
+                    string s1 = "", s2 = "";
+                    while (i< arr1.Length && char.IsDigit(arr1[i]))
+                    {
+                        s1 += arr1[i];
+                        i++;
+                    }
+                    while (j < arr2.Length && char.IsDigit(arr2[j]))
+                    {
+                        s2 += arr2[j];
+                        j++;
+                    }
+                    if (int.Parse(s1)< int.Parse(s2))
+                    {
+                        return 1;
+                    }else if(int.Parse(s1)> int.Parse(s2))
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    if (arr1[i] < arr2[j])
+                    {
+                        return 1;
+                    }if (arr1[i] > arr2[j])
+                    {
+                        return -1;
+                    }
+                    i++;
+                    j++;
+                }
+            }if (arr1.Length == arr2.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return arr1.Length > arr2.Length ? 1 : -1;
+            }
+        }
+    }
+}
